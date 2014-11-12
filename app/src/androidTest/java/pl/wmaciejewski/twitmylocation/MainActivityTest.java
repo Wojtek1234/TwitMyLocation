16c1490bd1e564e7f4acdfde43e933007d40be66
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-11-12 22:43:29 +0100

    added some basics test for main activity

---
+++ app/src/androidTest/java/pl/wmaciejewski/twitmylocation/MainActivityTest.java
@@ -0,0 +1,61 @@
+package pl.wmaciejewski.twitmylocation;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.test.ActivityUnitTestCase;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Wojtek on 2014-11-12.
+ */
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+
+    private MainActivity activity;
+
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Intent intent = new Intent(getInstrumentation().getTargetContext(),
+                MainActivity.class);
+        startActivity(intent, null, null);
+        activity = getActivity();
+    }
+
+
+    public void testUI(){
+        Fragment mapFragment=activity.findViewById(R.id.mapFragment);
+        assertNotNull(mapFragment);
+
+        LinearLayout tweeterLayout=activity.findViewById(R.id.tweetingPanel);
+        assertTrue(tweeterLayout.getVisibility()== View.GONE);
+
+        LinearLayout mapLayout=activity.findViewById(R.id.mapPanel);
+        assertTrue(mapLayout.getVisibility()== View.GONE);
+    }
+
+    public void testOnClicSignUpButton(){
+        Button loginButton=activity.findViewById(R.id.loginButton);
+        loginButton.performClick();
+        final Intent launchIntent = getStartedActivityIntent();
+        assertNotNull(launchIntent);
+        assertEquals(launchIntent.getClass().getName(),RequestTokenActivity.class.getName());
+    }
+
+    public void testSetPositionOnMap(){
+
+    }
+
+    public void testGetLocalImage(){
+
+    }
+
+    public void testSendTwitButton(){
+
+    }
+}
