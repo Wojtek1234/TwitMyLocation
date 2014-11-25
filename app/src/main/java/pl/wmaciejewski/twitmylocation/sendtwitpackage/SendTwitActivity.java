f90c410bba1d335b0e56e7944a49de97f043665d
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-11-25 11:11:22 +0100

    sending image with twitter and geoLock works!!!!!

--- app/src/main/java/pl/wmaciejewski/twitmylocation/sendtwitpackage/SendTwitActivity.java
+++ app/src/main/java/pl/wmaciejewski/twitmylocation/sendtwitpackage/SendTwitActivity.java
@@ -2,6 +2,7 @@ package pl.wmaciejewski.twitmylocation.sendtwitpackage;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -150,7 +151,7 @@ public class SendTwitActivity extends FragmentActivity implements ImagePickerDia
         switch (requestCode) {
             case SELECT_PHOTO_GALLERY:
                 if (resultCode == RESULT_OK) {
-                    doOnUriRecived(imageReturnedIntent.getData());
+                    file=new File(getRealPathFromUri(imageReturnedIntent.getData()));
                 }
                 break;
             case SELECT_PHOTO_CAPTURE:
@@ -163,8 +164,22 @@ public class SendTwitActivity extends FragmentActivity implements ImagePickerDia
     }
 
     private void doOnUriRecived(Uri selectedImage) {
-        file = new File(selectedImage.toString());
+        file = new File(selectedImage.getPath());
     }
 
+    private  String getRealPathFromUri(Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
 
 }
