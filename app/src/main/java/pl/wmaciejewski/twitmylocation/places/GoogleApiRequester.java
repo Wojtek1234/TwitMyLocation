ace8165f13f37545ffe5345b405b5bc016558270
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-11-30 16:33:14 +0100

    get rid of one button

--- app/src/main/java/pl/wmaciejewski/twitmylocation/places/GoogleApiRequester.java
+++ app/src/main/java/pl/wmaciejewski/twitmylocation/places/GoogleApiRequester.java
@@ -1,7 +1,9 @@
 package pl.wmaciejewski.twitmylocation.places;
 
-        import retrofit.Callback;
-        import retrofit.RestAdapter;
+import android.location.Location;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
 
 /**
  * Created by Wojtek M on 2014-11-29.
@@ -18,6 +20,20 @@ public class GoogleApiRequester {
                                    Callback<PlacesList> callback) {
         String placeParam=placeName+"+in+"+cityName;
         service.getPlacesForCity(placeParam,KEY,callback);
+
+    }
+    /*
+    *  public void getPlacesForLocation(@Query("location") String locationLatLng,@Query("radius") int radius
+           , @Query("name") String name ,@Query("key") String key,
+                                 Callback<PlacesList> callback);
+    *
+    * */
+    public void rquestNearBy(Location location,
+                                   int radius,
+                                   String what,
+                                   Callback<PlacesList> callback) {
+        String placeParam="location="+location.getLatitude()+","+location.getLongitude();
+        service.getPlacesForLocation(placeParam,radius,what,KEY,callback);
 
     }
 }
